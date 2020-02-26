a5c6e7dacb7abd25ae8552c12cb4738e434a5544
Rodolfo Santander <rodolfo.santander06@gmail.com>
2020-02-25 22:56:25 -0300

    Updates

---
+++ precache-manifest.dc2c9904338400260c1b10bc49f03c0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d1da980e487c44abe6dda99de587958",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "46b42ba90e055da0a565",
+    "url": "/monsters-rolodex/static/css/main.ee1b3483.chunk.css"
+  },
+  {
+    "revision": "f18ec448c3ab67b3b179",
+    "url": "/monsters-rolodex/static/js/2.9e4b70aa.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.9e4b70aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46b42ba90e055da0a565",
+    "url": "/monsters-rolodex/static/js/main.19821c72.chunk.js"
+  },
+  {
+    "revision": "24034436bd08b53a5646",
+    "url": "/monsters-rolodex/static/js/runtime-main.6386504b.js"
+  }
+]);
